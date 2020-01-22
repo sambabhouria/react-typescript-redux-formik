25d223bfdb858b09ba006e0b0264ea1ebffe6d2e
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-01-22 16:52:35 +0100

    adding typscript types

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,69 @@
 import React from "react";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "../css/base.css";
+
 const App = () => {
-  return <h1>Welcome to my todo react-typescrit app</h1>;
+  return (
+    <div>
+      <Router>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
 };
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
 
 export default App;
